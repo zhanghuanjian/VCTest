26c6bfecb91a56ef63d6a36d049e9555d8058d03
zhanghuanjian <2027663096@qq.com>
2020-06-05 21:01:39 +0800

    2020.06.05提交

---
+++ 实验8-2-1简易计算器/实验8-2-1简易计算器/实验8-2-1简易计算器.h
@@ -0,0 +1,32 @@
+
+// 实验8-2-1简易计算器.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// C实验821简易计算器App: 
+// 有关此类的实现，请参阅 实验8-2-1简易计算器.cpp
+//
+
+class C实验821简易计算器App : public CWinApp
+{
+public:
+	C实验821简易计算器App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern C实验821简易计算器App theApp;
