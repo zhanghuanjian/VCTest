b6fa9ab32e0d39fe59b0a350d4efafe455eb8ff6
zhanghuanjian <2027663096@qq.com>
2020-06-08 08:36:34 +0800

    综合实验之三

---
+++ CTest13-综3/CTest13-2MFC数据库的增删查改/CTest13-2MFC数据库的增删查改Doc.h
@@ -0,0 +1,49 @@
+
+// CTest13-2MFC数据库的增删查改Doc.h : CCTest132MFC数据库的增删查改Doc 类的接口
+//
+
+
+#pragma once
+#include "CTest13-2MFC数据库的增删查改Set.h"
+
+
+class CCTest132MFC数据库的增删查改Doc : public CDocument
+{
+protected: // 仅从序列化创建
+	CCTest132MFC数据库的增删查改Doc();
+	DECLARE_DYNCREATE(CCTest132MFC数据库的增删查改Doc)
+
+// 特性
+public:
+	CCTest132MFC数据库的增删查改Set m_CTest132MFC数据库的增删查改Set;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL OnNewDocument();
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+// 实现
+public:
+	virtual ~CCTest132MFC数据库的增删查改Doc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// 用于为搜索处理程序设置搜索内容的 Helper 函数
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
